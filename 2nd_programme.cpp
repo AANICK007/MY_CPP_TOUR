bbf64027941cd5c4af80037a8629f673905fd3b7
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-18 09:15:04 +0530

    Create 2nd_programme.cpp

---
+++ 2nd_programme.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+    string name ;
+    cout << "May I know your name = " ;
+    getline (cin, name ) ;
+    cout << "Hello , dear  "  << name ;
+    return 0 ;
+}
