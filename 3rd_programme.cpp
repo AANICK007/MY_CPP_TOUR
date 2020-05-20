d98d73167341cf774d058653c1d28cbca376a934
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-20 07:44:02 +0530

    Create 3rd_programme.cpp

---
+++ 3rd_programme.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std ;
+
+int main()
+{
+	int B , H ;
+	float A ; 
+	cout << " Enter the value of base =  " ;
+	cin >> B ;
+	cout << " Enter the value of height = " ;
+	cin >> H ;
+	A = (float)(0.5) * B * H  ;
+	cout << " The area of triangle formed by given sides = " << A ;
+	return 0 ;
+}
