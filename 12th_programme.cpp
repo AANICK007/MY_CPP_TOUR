69b0013abf87cf51e017d92f5655a6a7c9498e3e
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-21 10:02:36 +0530

    Create 12th_programme.cpp

---
+++ 12th_programme.cpp
@@ -0,0 +1,16 @@
+// This a programme to find greater among two numbers
+#include<iostream>
+
+using namespace std ;
+int main(){
+	int a , b ;
+	cout << "Enter two numbers a and b respectively =  " ;
+	cin >> a >> b ;
+	if ( a > b ){
+		cout << "a is greater than b " ;
+	}
+	else {
+		cout << "b is greater than a " ;
+	}
+	return 0 ;
+}
