1e64f27f47af4a529b41e891f3007633aee514ce
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-22 18:58:41 +0530

    Create pattern_5.cpp

---
+++ pattern_5.cpp
@@ -0,0 +1,26 @@
+// This is a programme to print the following pattern
+/* 
+    *****
+     ****
+      ***
+       **
+        *
+*/        
+   
+#include<iostream>
+
+using namespace std ;
+int main() {
+	for ( int i = 1 ; i <= 5 ; i++ ) {
+		for ( int j = 1 ; j <= 5 ; j++ ) {
+			if ( j >= i ){
+				cout << "*" ;
+			}
+			else {
+				cout << " " ; 
+			}
+		}
+		cout << "\n" ;
+	}
+	return 0 ;
+}
