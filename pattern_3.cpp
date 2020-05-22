011693a4244fba870b53d1c92485399205d43ddc
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-22 18:58:13 +0530

    Create pattern_3.cpp

---
+++ pattern_3.cpp
@@ -0,0 +1,22 @@
+// This is a programme to print the following pattern
+/* 
+     1 2 3 4 5
+     6 7 8 9 10
+     11 12 13 14 15
+     16 17 18 19 20 
+     21 22 23 24 25
+*/     
+#include<iostream>
+
+using namespace std ;
+int main() {
+	int count = 1 ;
+	for ( int i = 1 ; i <= 5 ; i++ ) {
+		for ( int j = 1 ; j <= 5 ; j++ ) {
+			cout << count << " " ;
+			count ++ ;
+		}
+		cout << "\n" ;
+	}
+	return 0 ;
+}
