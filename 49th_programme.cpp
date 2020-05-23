025a1423ca79c32780be1b172dc350cb10fac464
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-23 18:00:44 +0530

    Create 49th_programme.cpp

---
+++ 49th_programme.cpp
@@ -0,0 +1,12 @@
+// This is a programme to declare srinngs through string class
+#include<iostream>
+#include<string>
+
+using namespace std ;
+int main() {
+	string str ;
+	cout << "Enter a string = " ;
+	getline(cin,str) ;
+	cout << str << endl ;	
+	return 0 ;
+}
