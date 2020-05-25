6b1e507bb392cb79836ba7e9fd9040a3f99ddbe1
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-25 07:04:02 +0530

    Create 58th_programme.cpp

---
+++ 58th_programme.cpp
@@ -0,0 +1,14 @@
+// This is a programme to understand the basics of functions
+#include<iostream>
+
+using namespace std ;
+void Display () ;
+
+int main() {
+	Display() ;
+	return 0 ;
+}
+
+void Display() {
+	cout << "Hello guys " << endl ;
+}
