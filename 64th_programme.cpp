4db96eb5c1917683e0824d742dc5aa63a427c739
ANIKET PATHAK <aanickpathak@gmail.com>
2020-05-25 08:19:56 +0530

    Create 64th_programme.cpp

---
+++ 64th_programme.cpp
@@ -0,0 +1,33 @@
+// This is a programme to find maximum of given numbers using function temolates
+#include<iostream> 
+
+using namespace std ;
+
+template<class T>
+T Max ( T x , T y ) {
+	return ( x > y ? x : y ) ;
+}
+
+int main() {
+	int a , b , M1 ;
+	cout << "Enter a number = " ;
+	cin >> a ;
+	cout << "Enter second number = " ;
+	cin >> b ;
+	
+	M1 = Max(a,b) ;
+	cout << "The maximum of both numbers = " << M1 << endl ;
+	
+	float i , j , M2 ;
+	cout << "\nEnter a float value = " ;
+	cin >> i ;
+	cout << "Enter next float value = " ;
+	cin >> j ;
+	
+	M2 = Max(i,j) ;
+	cout << "The maximum of both values = " << M2 <<endl ;
+	 
+	return 0 ;
+}
+
+
